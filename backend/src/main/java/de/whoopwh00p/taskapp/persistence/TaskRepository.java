1f56b8d7a37c9fda5556cfe87d3b8bc7116d5d4d
whoopwh00p <stefanczubek@web.de>
2020-06-06 18:12:19 +0200

    Update TaskRepository.java

--- backend/src/main/java/de/whoopwh00p/taskapp/persistence/TaskRepository.java
+++ backend/src/main/java/de/whoopwh00p/taskapp/persistence/TaskRepository.java
@@ -4,8 +4,6 @@ import de.whoopwh00p.taskapp.model.Task;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Integer> {
     Task findByName(String name);
