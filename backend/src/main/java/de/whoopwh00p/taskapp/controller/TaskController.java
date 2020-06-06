c12adb8afa456ec1f41f2f873d22e060345782ea
whoopwh00p <stefanczubek@web.de>
2020-06-06 15:38:08 +0200

    add Logger

--- backend/src/main/java/de/whoopwh00p/taskapp/controller/TaskController.java
+++ backend/src/main/java/de/whoopwh00p/taskapp/controller/TaskController.java
@@ -4,9 +4,13 @@ import de.whoopwh00p.taskapp.model.Task;
 import de.whoopwh00p.taskapp.persistence.TaskRepository;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/tasks")
 public class TaskController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskController.class);
 
     private final TaskRepository taskRepository;
 
@@ -17,6 +21,7 @@ public class TaskController {
 
     @Get("/findAll")
     public Iterable<Task> findAll() {
+        LOGGER.info("findAll invoked.");
         return this.taskRepository.findAll();
     }
 
