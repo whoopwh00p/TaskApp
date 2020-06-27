52a6b9f73fbc740023c6689b0fcdf3c0eb0d3838
whoopwh00p <stefanczubek@web.de>
2020-06-27 20:35:22 +0200

    TAP-28: remove example components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { AuthGuard } from './auth/auth.guard';
 import { CallbackComponent } from './callback.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'callback', component: CallbackComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent, canActivate: [AuthGuard] },
-  { path: 'heroes', component: HeroesComponent, canActivate: [AuthGuard] }
+  { path: 'callback', component: CallbackComponent }
 ];
 
 @NgModule({
--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -3,7 +3,6 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angul
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
-import { MessageService } from '../message.service';
 
 
 @Injectable()
@@ -12,13 +11,11 @@ export class AuthGuard implements CanActivate {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private messageService: MessageService
   ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    this.messageService.add('CanActivate'+this.authService.isLoggedIn)
     if (!this.authService.isLoggedIn) {
       this.router.navigate(['/']);
       return false;
